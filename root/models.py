e6947cee99d193b52aa3603e187f7354574a7dad
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-01-06 17:12:29 +0330

    models

--- root/models.py
+++ root/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Service(models.Model):
+    pass
+
+
+
+class Menu(models.Model):
+    pass
+
+
+
+
+class MenuSpecials(models.Model):
+    pass
+
+
+
+
+class EventsRestaurant(models.Model):
+    pass
+
+
+
+
+class Order(models.Model):
+    pass
+
+
+
+
+class Comment(models.Model):
+    pass
+
+
+
+class PhotosRestaurant:
+    pass
+
+
+
+class Chefs(models.Model):
+    pass
+
+
+class ContactUs (models.Model):
+    pass
+
+
+
+
+
+
