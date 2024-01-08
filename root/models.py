4af4eb6aaafd4529ea4ca7a5d662c2e354b33470
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-01-08 12:43:06 +0330

    models

--- root/models.py
+++ root/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from accounts.models import CustomeUser
+import datetime
 # Create your models here.
 
 
@@ -12,11 +13,18 @@ class Service(models.Model):
         return self.title
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
 class Menu(models.Model):
     image = models.ImageField(upload_to='Menu',default='Menu.jpg')
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
@@ -30,6 +38,7 @@ class MenuSpecials(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=200)
     content2 = models.CharField(max_length=200)
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
@@ -52,28 +61,70 @@ class EventsRestaurant(models.Model):
 
 
 class Order(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    number= models.IntegerField()
+    message = models.TextField()
+    date = models.DateTimeField()
+    time= models.DateTimeField()
+
+    def __str__(self):
+        return self.email
+    
 
 
 
 
-class Comment(models.Model):
-    pass
+
+# class Comment(models.Model):
+#     pass
 
 
 
 class PhotosRestaurant:
-    pass
+    gallery = models.ImageField(upload_to='Gallery')
 
+
+
+
+class Skills(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 
 class Chefs(models.Model):
-    pass
+    info = models.ForeignKey(CustomeUser , on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    skills = models.ManyToManyField(Skills)
+    image = models.ImageField(upload_to='trainer', default='teacher.png')
+    twitter = models.CharField(max_length=255, default='#')
+    facebook = models.CharField(max_length=255, default='#')
+    instagram = models.CharField(max_length=255, default='#')
+    linkdin = models.CharField(max_length=255, default='#')
+    status = models.BooleanField(default=False)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.info.email
 
 
 class ContactUs(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
 
+    def __str__(self):
+        return self.name
+    
+
+class RestaurantInformation(models.Model):
+    location = models.CharField(max_length=250)
+    open_hours:models.DateTimeField(default=datetime.datetime.now)
+    email:models.EmailField()
+    call:models.IntegerField(max_length=15)
 
 
 
